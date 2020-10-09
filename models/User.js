ab7ea3a20830742c766d268b7a2aba5e2f238296
purvaarte <69966169+purvaarte@users.noreply.github.com>
2020-10-09 13:21:47 +1300

    user updated

---
+++ models/User.js
@@ -0,0 +1,6 @@
+import EventSelector from "./Components/EventSelector";
+import Slider from "@material-ui/lab/Slider";
+import { data } from "./data/data.js";
+// import { data as personalData } from "./data/personal2.js";
+    this.updateRange = this.updateRange.bind(this);
+    this.dataSources = { ...data };
